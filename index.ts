57250377383d449986af0aaca74d21a16f417ade
Bryn Dyment <bryndyment@gmail.com>
2025-02-19 09:35:32 +0900

    1.0.2

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import { useCallback, useState } from 'react'
 // hooks
 
 export const useRefCallback = <T extends HTMLElement>() => {
-  const [element, setElement] = useState<null | T>(null)
+  const [element, setElement] = useState<T | null>(null)
 
-  const refCallback = useCallback((node: null | T) => setElement(node), [])
+  const refCallback = useCallback((node: T | null) => setElement(node), [])
 
-  return { element, refCallback }
+  return [element, refCallback]
 }
